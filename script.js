44a0480f9250aac62885a22915d4ce0b9458b86f
Mateus Filho <100981669+mateus-filho@users.noreply.github.com>
2023-05-21 08:21:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,3 @@ function abrirmenu() {
 	}
 }
 
-const links = document.querySelectorAll(".scroll-link");
-
-// Para cada link, adiciona um evento de clique que rola a página suavemente para a
-// seção correspondente
-links.forEach((link) => {
-	link.addEventListener("click", (event) => {
-		event.preventDefault();
-
-		const sectionId = link.getAttribute("href").substring(1);
-		const section = document.getElementById(sectionId);
-
-		window.scrollTo({
-			top: section.offsetTop,
-			behavior: "smooth",
-		});
-	});
-});
